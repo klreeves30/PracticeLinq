6be27fd658fadde178ad342010820320abc9008c
klreeves30 <klreeves30@gmail.com>
2021-11-15 11:55:59 -0500

    inital commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        /// Created a list for video games
+        /// created a variable "lengthOfNames by using Lambad expression to get order and length to list"
+        /// created a foreach loop in list to print out list of name by length in decending order.
+
+        static void Main(string[] args)
+        {
+            List<string> listOfGames = new List<string> { "Madden", "Resident Evil", "NBA 2K", "PGA Golf" };
+
+            var lengthOfNames = listOfGames.OrderByDescending(name => name.Length).ToList();
+
+            foreach (var i in lengthOfNames)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
